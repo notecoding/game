eb05b52301e65731f2d37f2ff16b176c2f09e16a
notecoding <63888297+notecoding@users.noreply.github.com>
2025-09-12 10:55:13 +0900

    Update PositionControl2.c

--- 0912/PositionControl2.c
+++ 0912/PositionControl2.c
@@ -1,6 +1,9 @@
  #include <stdio.h>
  #include <windows.h>
- void gotoxy(int x, int y);
+ void gotoxy(int x,int y){
+    COORD Pos={x-1,y-1};
+    SetConsoleCursorposition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
+}
  int main(void)
  {
  for(int i=1;i<=9;i++)
@@ -11,4 +14,5 @@
  printf("\n");
  return 0;
  }
+
  //3단 출력
