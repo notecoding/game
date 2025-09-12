c5a81781856d280c33395040348daaa900ef71bf
notecoding <63888297+notecoding@users.noreply.github.com>
2025-09-12 11:07:13 +0900

    Update move1.c

--- 0912/move1.c
+++ 0912/move1.c
@@ -27,3 +27,24 @@ void gotoxy(int x,int y){
     COORD Pos={x-1,y-1};
     SetConsoleCursorposition(GetStdHandle(STD_OUTPUT_HANDLE), Pos);
 }
+#include<stdio.h>
+ #include<conio.h>
+ #include<windows.h>
+ #define X_MAX 79  //가로(열)방향의최대값
+#define Y_MAX 24  //세로(행)방향의최대값
+void move_arrow_key(char chr, int *x, int *y, int x_b, int y_b);
+ void gotoxy(int x, int y);
+ int main(void)
+ {
+ char key;
+ int x=10, y=5;
+ do
+ {
+ gotoxy(x, y);
+ printf("A");
+ key=getch();
+ move_arrow_key(key, &x, &y, X_MAX, Y_MAX);
+ }while(key!=27);
+ return 0;
+ }
+
